6cea6b98cb62414663ef0739f0edddd12b4c0ae5
akashkumar6205 <akashkumar23012005@gmail.com>
2026-09-22 22:23:05 +0530

    Update dependencies and improve form error handling in Create component

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -10,7 +10,7 @@ const {
     register,  //for two way binding
     handleSubmit, //submission
     reset, //for reset form
-    formState: {error}, //for finding error 
+    formState: {errors}, //for finding error 
 } = useForm()
 
 
@@ -22,25 +22,25 @@ const submitHandler = (data) => {
 const copytodos = [...todos];
     copytodos.push(data);
     settodos(copytodos);
-
+    
+// for clear the title
     reset();
 }
 
-
-
-
-  return (
+return (
     <div className=" w-[60%] p-10 ">
         <h1 className="mb-10 text-5xl font-thin">
             Set <span className="text-red-500">Reminder</span> For <br/>Task 
             </h1>
             <form onSubmit={handleSubmit(submitHandler)}>
                 <input
-                    {...register("title")}
+                    {...register("title", 
+                        {required: "title can not be empty"})}
                     className="p-2 border-b w-full text-2xl font-thin outline-0"
                     type= "text" 
                     placeholder="title" 
                 />
+                <small className="font-thin text-red-400">{errors?.title?.message}</small>
                 <br />
                 <br />
                     <button className=" mt-5 text-xl px-10 py-2 border rounded">Create Todo</button>
